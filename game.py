02e7fbadde704171ee337787af9422415b87ad0d
tcstorrancecalebkcoder <151399804+tcstorrancecalebkcoder@users.noreply.github.com>
2024-06-25 18:16:30 -0700

    Worked out Kinks
    we worked out and organized things to work

--- game.py
+++ game.py
@@ -6,17 +6,20 @@ class Game:
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((1000, 800))
-        self.game_obj = GameObj()
+        self.game_obj = GameObj(0, 0, 50 ,50)
         self.main_game_loop()
         
     def main_game_loop(self):
         while True: 
             self.event_handler()
-            self.game_obj.draw()
             self.draw()
-            
+
     def event_handler(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+    def draw(self):
+        self.game_obj.draw(self.window)
+        pygame.display.update()
--- game_obj.py
+++ game_obj.py
@@ -2,12 +2,11 @@ import pygame
 
 class GameObj:
 
-    def __init__(self, x, y. width, height):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
 
     def draw(self, window):
-        pygame.draw.rect(window, (0, 255, 0), pygame.Rect(500, 400, 20, 20))
-        pygame.display.update()
+        pygame.draw.rect(window, (0, 255, 0), pygame.Rect(self.x, self.y, self.width, self.height))
