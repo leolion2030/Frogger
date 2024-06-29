e91c6d46708c1c5eb4d47dcae8e816e75e08806d
tcstorrancecalebkcoder <151399804+tcstorrancecalebkcoder@users.noreply.github.com>
2024-06-29 12:10:51 -0700

    Movement
    We were able to make the character move straight.

--- game.py
+++ game.py
@@ -6,12 +6,17 @@ class Game:
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((1000, 800))
+        self.clock = pygame.time.Clock()
+        self.fps = 60
         self.game_obj = GameObj(0, 0, 50 ,50)
+        self.player = GameObj(500, 500, 20, 20)
         self.main_game_loop()
         
     def main_game_loop(self):
         while True: 
+            self.clock.tick(self.fps)
             self.event_handler()
+            self.key_handler()
             self.draw()
 
     def event_handler(self):
@@ -22,15 +27,16 @@ class Game:
 
     def draw(self):
         self.game_obj.draw(self.window)
+        self.player.draw(self.window)
         pygame.display.update()
 
     def key_handler(self):
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_w] == True:
-            self.player.y -= self.player.speed
+            self.player.y -= 5
         elif pressed_keys[pygame.K_s] == True:
-            self.player.y += self.player.speed
+            self.player.y += 5
         elif pressed_keys[pygame.K_a] == True:
-            self.player.x -= self.player.speed
+            self.player.x -= 5
         elif pressed_keys[pygame.K_d] == True:
-            self.player.x += self.player.speed
+            self.player.x += 5
