4b70f99587e110c06166e2f0cb00456f43fb4fc6
leolion2030 <167942327+leolion2030@users.noreply.github.com>
2024-07-02 17:43:43 -0700

    Car and MOtion
    I was unable to get the movement to be straight and had the car move

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ class Game:
         self.fps = 60
         self.game_obj = GameObj(0, 0, 50 ,50)
         self.player = GameObj(500, 500, 20, 20)
+        self.car = GameObj(0, 400, 60, 20 )
         self.main_game_loop()
         
     def main_game_loop(self):
@@ -17,6 +18,7 @@ class Game:
             self.clock.tick(self.fps)
             self.event_handler()
             self.key_handler()
+            self.move()
             self.draw()
 
     def event_handler(self):
@@ -28,15 +30,22 @@ class Game:
     def draw(self):
         self.game_obj.draw(self.window)
         self.player.draw(self.window)
+        self.car.draw(self.window)
         pygame.display.update()
 
     def key_handler(self):
+        wait = pygame.time.get_ticks()
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_w] == True:
             self.player.y -= 5
+            if wait >= 1999:
+                
         elif pressed_keys[pygame.K_s] == True:
             self.player.y += 5
         elif pressed_keys[pygame.K_a] == True:
             self.player.x -= 5
         elif pressed_keys[pygame.K_d] == True:
             self.player.x += 5
+
+    def car_move(self):
+        self.car.x += 10
